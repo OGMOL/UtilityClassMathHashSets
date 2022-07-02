2dbf13db0065b56a52b80c894b5493685bcc38d7
OGMOL <olegmolokov11@gmail.com>
2022-07-02 14:32:59 +0300

    "add UtilityClass and methods whit generics"

--- src/main/java/MathHashSets.java
+++ src/main/java/MathHashSets.java
@@ -1,3 +1,27 @@
-public class MathHashSets {
+import java.util.HashSet;
 
+public class MathHashSets {
+    public static <T> HashSet<T> union(HashSet<T> first, HashSet<T> second) {
+        first.addAll(second);
+        return first;
+    }
+
+    public static <T> HashSet<T> intersection(HashSet<T> first, HashSet<T> second) {
+        first.retainAll(second);
+        return first;
+    }
+
+    public static <T> HashSet<T> minus(HashSet<T> first, HashSet<T> second) {
+        first.removeAll(second);
+        return first;
+    }
+
+    public static <T> HashSet<T> difference(HashSet<T> first, HashSet<T> second) {
+        HashSet<T> res = new HashSet<>(first);
+        HashSet<T> res2 = new HashSet<>(second);
+        res.removeAll(second);
+        res2.removeAll(first);
+        res.addAll(res2);
+        return res;
+    }
 }
---
+++ src/test/java/MathHashSetsTest.java
@@ -0,0 +1,32 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MathHashSetsTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void union() {
+    }
+
+    @Test
+    public void intersection() {
+    }
+
+    @Test
+    public void minus() {
+    }
+
+    @Test
+    public void difference() {
+    }
+}
